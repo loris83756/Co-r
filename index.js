a00be33ae6e38c2872b2969a7641706c4b678dc9
loris83756 <43618944+loris83756@users.noreply.github.com>
2018-11-23 15:06:18 +0100

    Update index.js

--- index.js
+++ index.js
@@ -40,5 +40,6 @@ bot.on('message', msg => {
 
 });
 
-bot.login(cfg.token); //a garder en version desktop
+bot.login(token); //a garder en version desktop
 bot.login(token); //a garder en version heroku
+const token = pocess.env.token
