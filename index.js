2d8d1c9ec50d7c12598e8f6c8a242981cefdb102
loris83756 <43618944+loris83756@users.noreply.github.com>
2018-11-25 20:07:41 +0100

    Update index.js

--- index.js
+++ index.js
@@ -11,9 +11,9 @@ bot.on('ready',function() {
 bot.login("process.env.TOKEN);
 
 
-bot.on ('message',message => {
+bot.on ('message', message => {
     if (message.content === prefix + "help"){
-        message.channel.senMessage5"Liste des commandes: \n -*help");
+        message.channel.senMessage("Liste des commandes: \n -*help");
     }
 
     if (message.content,=== "Salut"){
