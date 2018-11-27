e32930e55ed7965180fc716a720d236c08895cbc
loris83756 <43618944+loris83756@users.noreply.github.com>
2018-11-27 22:20:01 +0100

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ bot.on('ready',function() {
 
 bot.on('message', message => {
     if (message.content === prefix + "help"){
-        message.channel.sendMessage("**Liste des commandes**: \n ```*informations:``` Donne des informations sur le bot.")
+        message.channel.sendMessage("**Liste des commandes**: \n ```*informations:``` Donne des informations sur le bot. \n \n ```*ban (bientôt disponible)``` \n Permet de banir des personnes")
     }
     
     if (message.content === prefix + "informations"){
