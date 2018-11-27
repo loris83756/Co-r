e07702b84988cbafc5d18971378294bdc97a5ac2
loris83756 <43618944+loris83756@users.noreply.github.com>
2018-11-27 20:16:25 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const bot = new Discord.Client();
 var prefix = ("*")
 
 bot.on('ready',function() {
-    bot.user.ClientUser#setActivity("Commande: *help");
+    bot.Clientuser.setActivity("Commande: *help");
     console.log ("Connectedç");
 });
 
