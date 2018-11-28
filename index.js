8f55a2b7afda08353869d7e59a00c9e54b455c0f
loris83756 <43618944+loris83756@users.noreply.github.com>
2018-11-28 17:15:18 +0100

    Update index.js

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ bot.on('message', message => {
 
    module.exports.run = async (bot, message, args) => {
 
-    message.channel.send("Pong: " + (message.createdTimestamp - Date.now()) + "ms");
+    message.channel.send("Pong: " + (Date.now() - message.createdTimestamp) + "ms");
 
    }
 
