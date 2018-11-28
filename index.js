8702567fce756a887f830f0e81012f30b9a33f26
loris83756 <43618944+loris83756@users.noreply.github.com>
2018-11-28 21:08:51 +0100

    Update index.js

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ bot.on('ready',function() {
 bot.on('message', message => {
     if (message.content === prefix + "help"){ 
         message.author.send("**Liste des commandes**: \n ```*informations: (nouveau)``` Donne des informations sur le bot.\n \n ```*ping: (nouveau)``` Donne le temps de réaction du bot en ms. \n \n ```*ban: (bientôt disponible)``` Permet de banir des personnes. \n \n ```*kick: (bientôt disponible)``` Permet d´expulser des personnes. \n \n ```*mute: (bientôt disponible)``` Permet de mute des personnes. ")
-        message.channel.sendMessage("Les commandes vous on était envoyé en MP")
+        message.channel.sendMessage("**Les commandes vous on était envoyé en MP :bee: **")
     }
     
     if (message.content === prefix + "informations"){
@@ -42,7 +42,7 @@ bot.on('message', message => {
 
 
     if (message.content === prefix + "ping"){
-        message.channel.sendMessage(`**Pong** ${message.createdTimestamp - Date.now()} ms`);
+        message.channel.sendMessage(`**Pong:** ${message.createdTimestamp - Date.now()} ms`);
     }
     
 });
