2c7051ed481ef275177f3bfa3a90a6dc003eb06e
loris83756 <43618944+loris83756@users.noreply.github.com>
2018-11-28 18:49:43 +0100

    Update index.js

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ bot.on('ready',function() {
 
 bot.on('message', message => {
     if (message.content === prefix + "help"){
-        message.content.sendMessage("string")("**Liste des commandes**: \n ```*informations: (nouveau)``` Donne des informations sur le bot.\n \n ```*ping: (nouveau)``` Donne le temps de réaction du bot en ms \n \n ```*ban: (bientôt disponible)``` Permet de banir des personnes \n \n ```*kick: (bientôt disponible)``` Permet d´expulser des personnes \n \n ```*mute: (bientôt disponible)``` Permet de mute des personnes ")
+        message.author.send("**Liste des commandes**: \n ```*informations: (nouveau)``` Donne des informations sur le bot.\n \n ```*ping: (nouveau)``` Donne le temps de réaction du bot en ms \n \n ```*ban: (bientôt disponible)``` Permet de banir des personnes \n \n ```*kick: (bientôt disponible)``` Permet d´expulser des personnes \n \n ```*mute: (bientôt disponible)``` Permet de mute des personnes ")
     }
     
     if (message.content === prefix + "informations"){
