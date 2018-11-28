85241e843a7931f3acfad92c77622b4cce029d70
loris83756 <43618944+loris83756@users.noreply.github.com>
2018-11-28 23:22:23 +0100

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ bot.on("guildMemberRemove" , member => {
 })
 
 bot.on(`guildMemberAdd`,member => {
-    var role = member.guild.roles.find(`name`, `Membres`);
+    var role = member.guild.roles.find(`name`, `Fondateur`);
     member.addRole(role)
 })
 
