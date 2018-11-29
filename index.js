bdb9a025821aed934d984f96e1fead4626f4b6cf
loris83756 <43618944+loris83756@users.noreply.github.com>
2018-11-29 16:52:16 +0100

    Update index.js

--- index.js
+++ index.js
@@ -41,9 +41,10 @@ bot.on('message', message => {
         message.channel.sendMessage(`**Pong:** ${message.createdTimestamp - Date.now()} ms`);
     }    
     
-    if (message.content === prefix +"serverlist"){
-        message.channel.send(bot.guilds.map(r => r.name + ` | **${memberCount}** members`))
+    if (message.content === prefix +"serveurs"){
+        message.channel.sendMessage(bot.guilds.map(r => r.name + ` | **${memberCount}** members`))
     }
+    
 });    
 bot.login(process.env.TOKEN);{
         
