251beb3f49ca64d587aa5533bb7ca419a531f017
loris83756 <43618944+loris83756@users.noreply.github.com>
2018-12-12 22:33:00 +0100

    Update index.js

--- index.js
+++ index.js
@@ -53,7 +53,7 @@ bot.on('message', message => {
     }
     
     if(message.content === prefix + "avatar"){
-      message.channel.sendMessage(`**Voici ton avatar.**`(message.author.avatarURL));
+      message.channel.sendMessage(message.author.avatarURL);
     }
     
 });
