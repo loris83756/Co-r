1877436730006aa0c0cec4e37b391b2bf7d5db0c
loris83756 <43618944+loris83756@users.noreply.github.com>
2019-01-03 14:45:21 +0100

    Update index.js

--- index.js
+++ index.js
@@ -57,7 +57,8 @@ bot.on('message', message => {
     }
     
     if (message.content === prefix +"off"){
-       message.author.id === '295211285405237248'
+        message.channel.sendMessage(`**Seul loris83756#0001 peut utiliser cette commande.**`)
+       if (message.author.id === '295211285405237248')
           message.channel.sendMessage(`**Arret en cours ...**`) 
       bot.user.setGame("Arret en cours ...");{
     }
