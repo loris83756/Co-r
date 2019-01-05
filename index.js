c0aab2c03febfa07d344f69c5f216f9df49c7f16
loris83756 <43618944+loris83756@users.noreply.github.com>
2019-01-05 16:19:17 +0100

    Update index.js

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ bot.on('message', message => {
     }
 
      if (message.content === prefix + "ping"){
-        message.channel.sendMessage(`:ping_pong:Pong: ${message.createdTimestamp - Date.now()} ms`)
+        message.channel.sendMessage(`:ping_pong: Pong: ${bot.ping} ms`)
     }
     
     if (message.content === prefix + "invitation"){
