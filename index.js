22255b2a88c89c2dda87ac26c3c0f59dcc9b6e0e
loris83756 <43618944+loris83756@users.noreply.github.com>
2019-01-05 22:11:46 +0100

    Update index.js

--- index.js
+++ index.js
@@ -86,7 +86,8 @@ if (message.content === prefix + "help") {
     
     if (message.content === prefix + "avatar") {
         const embed = new RichEmbed()
-        .addField(`**Voici ton avatar:**`,`message.author.avatarURL`)
+        .setTitle(`**Voici ton avatar:**`)
+        .addImage(`message.author.avatarURL`)
         .setColor(0xffa500)
         .setFooter("Beeing")
         message.channel.send(embed);
