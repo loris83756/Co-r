5b45afa00060474cd8bb15ea76c4558b91e66736
loris83756 <43618944+loris83756@users.noreply.github.com>
2019-01-05 21:49:38 +0100

    Update index.js

--- index.js
+++ index.js
@@ -24,14 +24,46 @@ bot.on('ready',function() {
 
 
 bot.on('message', message => {
-    if (message.content === prefix + "help"){ 
-        message.author.send("**__Liste des commandes__::bee:** \n ```*informations: (nouveau)``` :small_orange_diamond: Donne des informations sur le bot.\n \n ```*ping: (nouveau)``` :small_orange_diamond: Donne le temps de réaction du bot en ms. \n \n ```*serveurs (nouveau)``` :small_orange_diamond: Donne la liste de tous les serveur ou se trouve Beeing#4440 \n \n ```*invitation (nouveau)``` :small_orange_diamond: Donne un lien permetant d'invité Beeing#4440 sur votre serveur \n \n ```*ban: (bientôt disponible)``` :tools: Permet de bannir des personnes. \n \n ```*kick: (bientôt disponible)``` :tools: Permet d´expulser des personnes. \n \n ```*mute: (bientôt disponible)``` :tools: Permet de mute des personnes. \n \n \n **__Autres fonctionnalités:__**:bee: \n ```salut``` :small_orange_diamond: Dites Salut et le Bot vous dira Bonjour. \n \n ```Bienvenue/a quitté le serveur``` :small_orange_diamond: Dès qu'une personne rejoindra votre serveur le Bot lui souhaitera la Bienvenue et à son départ le Bot dira @...#0000 a quitté le serveur. \n :exclamation:**Attention il faut posséder un salon nouveau/départ pour pouvoir avoir accès à cette fonctionnalité.**:exclamation: \n \n ```Auto role Membres``` :small_orange_diamond: Ajoute automatiquement le role **Membres** a tout les nouveaux de votre serveur \n :exclamation:**Attention il faut posséder le role _Membres_.**:exclamation:")
-        message.channel.sendMessage("**Les commandes vous on était envoyé en MP   **")
+if (message.content === prefix + "help") {
+    const embed = new RichEmbed()
+      .setTitle('**__Liste des commandes__::bee:** \n ')
+      .addField(`*informations`,`:small_orange_diamond:Donne des informations sur le bot.`)
+      .addField(`*ping`,`:small_orange_diamond: Donne le temps de réaction du bot en ms.`)
+      .addField(`*serveurs`,`:small_orange_diamond: Donne la liste de tous les serveurs et du nombre de membres ou se trouve Beeing#4440.`)
+      .addField(`*avatar`,`:small_orange_diamond:Donne ton avatar Discord. `)
+      .addField(`**__Liste des commandes de modérations__::bee:**`,)
+      .addField(`*ban (Bientot disponible)`,`:tools:Permet de bannir des utilisateurs.`)
+      .addField(`*kick (Bientot disponible)`,`:tools: Permet d´expulser des utilisateurs.`)
+      .addField(`*mute (Bientot disponible)`,`:tools: Permet de rendre muet des utilisateurs.`)
+      .addField(`**__Autres fonctionalitées:__**:bee:`)
+      .addField(`Salut`,`:small_orange_diamond:Dites Salut et le Bot vous dira Bonjour.`)
+      .addField(`Bienvenue/a quitté le serveur`,`:small_orange_diamond: Dès qu une personne rejoindra votre serveur le Bot lui souhaitera la Bienvenue et à son départ le Bot dira @...#0000 a quitté le serveur. \n :exclamation:Attention il faut posséder un salon nouveau-départ pour pouvoir avoir accès à cette fonctionnalité.:exclamation:`)
+      .addField(`Auto rôle Membres `,`:small_orange_diamond:Ajoute automatiquement le rôle Membres a tout les nouveaux de votre serveur. \n :exclamation: Attention il faut posséder le rôle Membres.:exclamation:`)
+      .setColor(0xffa500)
+      .setFooter("Beeing")
+    message.author.send(embed);
+  }
+    
+    if (message.content === prefix + "help") {
+     const embed = new RichEmbed()
+      .setTitle('Les commandes vous ont été envoyés en MP :mailbox_with_mail:')
+      .setColor(0xffa500)
+      .setFooter("Beeing")
+     message.channel.send(embed)
     }
     
-    if (message.content === prefix + "informations"){
-        message.channel.sendMessage("Je suis programmé par **loris83756#0001** en **JavaScript** et je suis hébergé chez **Heroku**.\n Beeing#4440 est sous licence ISC .")
-        console.log("Commande effectué");
+    if (message.content === prefix + "informations") {
+  const embed = new RichEmbed()
+                .setTitle(':information_source: **Informations:**')
+                .addField(`:satellite: Serveurs:`, `${bot.guilds.size}`)
+                .addField(`:grinning: Users`, `${bot.users.size}`)
+                .addField(`:vs: Version:`, `0.0.1 alpha`)
+                .addField(`:computer: Developpeur`, `loris83756#0001`)
+                .addField(`:pencil: Codage utilisé:`, `Node JS 11.4.2`)
+                .addField(`:regional_indicator_h: Hébergeur:`, `Heroku`, true)
+                .addField(`:ok_hand: Server Support:`, `[Cliquer ici](https://discord.gg/fERCmfe)`) 
+                .setColor(0xffa500);
+            message.channel.send(embed);
     }
     
     if (message.content === "Salut"){
@@ -43,26 +75,17 @@ bot.on('message', message => {
         message.channel.sendMessage(bot.guilds.map(r => r.name + ` |**${r.memberCount}** membres`))
     }
 
-     if (message.content === prefix + "ping"){
-        message.channel.sendMessage(`:ping_pong: Pong: ${bot.ping} ms`)
-    }
-    
-    if (message.content === prefix + "invitation"){
-        message.channel.sendMessage("**Voici le lien pour m'inviter sur ton serveur:** https://discordapp.com/api/oauth2/authorize?client_id=515518385686183937&permissions=0&scope=bot")
+  if (message.content === prefix + "ping") {
+    const embed = new RichEmbed()
+                .setTitle(`:ping_pong: Pong: ${bot.ping} ms`)
+                .setColor(0xffa500)
+                .setFooter("Beeing")
+     message.channel.send(embed);
     }
     
     if (message.content === prefix + "avatar"){
       message.channel.sendMessage(message.author.avatarURL)
     }
     
-    if (message.content === prefix + "off"){
-        message.channel.sendMessage(`**Seul loris83756#0001 peut utiliser cette commande.**`)
-       if (message.author.id === '295211285405237248')
-          message.channel.sendMessage(`**Arret en cours ...**`) 
-      bot.user.setGame("Arret en cours ...");{
-      }
-        ccrashhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh
-    }
-        
 }); 
 bot.login(process.env.TOKEN);
