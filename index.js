fd3bd214293a10768b80c8aefa1d0cbfd6208377
loris83756 <43618944+loris83756@users.noreply.github.com>
2019-01-26 18:28:16 +0100

    Update index.js

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ bot.on(`guildMemberAdd`,member => {
 })
 //SET GAME-----------------------------------------------------------------------------------------------------------------------
 bot.on('ready',function() {
-bot.user.setPresence({ game: { name: '*help', type: 1 } });
+bot.user.setPresence({ game: { name: '*help', type: 2 } });
     console.log ("Connected");
 });
 //HELP---------------------------------------------------------------------------------------------------------------------------
