6ffc0996e992a51dea7004061c7fd8fe6dc6b1cc
loris83756 <43618944+loris83756@users.noreply.github.com>
2019-01-26 18:11:33 +0100

    Update index.js

--- index.js
+++ index.js
@@ -111,7 +111,7 @@ message.channel.sendEmbed(embed)
 //SERVEURS-------------------------------------------------------------------------------------------------------------------------    
      if (message.content === prefix + "serveurs") {
          const embed = new RichEmbed()
-        .setTitle(bot.guilds.map(r => r.name + ` |**${r.memberCount}** membres`))
+        .setTitle(bot.guilds.map(r => r.name + ` |**${r.memberCount}** membres \n -----------------`))
         .setTimestamp()
         .setFooter("Beeing","https://cdn.discordapp.com/attachments/511129543768145930/531464974548205568/Beeing.jpg")
         .setColor(0xffa500);
