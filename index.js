28c01182c1d8465e00f3d5f59629a526fcf3d570
loris83756 <43618944+loris83756@users.noreply.github.com>
2019-01-27 13:45:13 +0100

    Update index.js

--- index.js
+++ index.js
@@ -238,8 +238,12 @@ message.channel.bulkDelete(clearargs[0]).then(() => {
     message.channel.send(`${clearargs[0]} messages ont était suprimer.`).then(msg => msg.delete(2000));
 })
 }
-          if(message.author.id === '295211285405237248' + "*off") 
-              return message.channel.send("**:x: Seul loris83756 peut utiliser cette commande**")
+    
+    if (message.content === prefix + "off") {
+        let id = message.author.id === '295211285405237247';
+        if(!id) {
+        return message.channel.send("**:x: Seul loris83756 peut utiliser cette commande**")
+        }
           bot.user.setGame("Arret en cours...") 
           }
           Crrrrrrraaassshhhshhshshshshshshsh
