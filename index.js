65740602f5a88873fa14b1bbd05ecba22de6365c
loris83756 <43618944+loris83756@users.noreply.github.com>
2019-02-16 11:34:20 +0100

    Update index.js

--- index.js
+++ index.js
@@ -351,7 +351,7 @@ console.log(`Commade sondage effectué par: ${message.author} sur ${message.guil
       .setColor(0xff0000)
       .setTimestamp()
       .setFooter(`Ecrit de: ${message.guild.name}`, `${message.guild.iconURL}`)
-       let iddes = message.author.id === '295211285405237248';{
+       let iddes = message.author.id === '295211285405237247';{
        if(!iddes)
        return bot.channels.findAll('name', 'd-e-s').map(channel => channel.send(desembed))
        console.log(`Commade D.E.S effectué par: ${message.author} sur ${message.guild.name} a ${message.createdAt}`)           
