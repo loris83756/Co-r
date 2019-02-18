c4cb241fd1a72a235538866fd066f01346eb8e32
loris83756 <43618944+loris83756@users.noreply.github.com>
2019-02-18 12:51:11 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,10 +3,6 @@ const bot = new Discord.Client();
 const { Client, RichEmbed } = require('discord.js');
 
 var prefix = ("-")
-
-if(message.content === prefix + "test") {
-  message.send("Test ok")
-}
     
 }); 
 bot.login(process.env.TOKEN);
