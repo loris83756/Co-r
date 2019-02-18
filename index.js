dd19194bbef99a60fc2ddce6f975a48a5bc33079
loris83756 <43618944+loris83756@users.noreply.github.com>
2019-02-18 12:45:38 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+const { Client, RichEmbed } = require('discord.js');
+
+var prefix = ("-")
+
+bot.login(proces.env.TOKEN)
 
