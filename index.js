84948deb50721f03004f24c4d96c0dc3daf67e16
loris83756 <43618944+loris83756@users.noreply.github.com>
2020-02-11 21:06:11 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,14 @@ const bot = new Discord.Client();
 const { Client, RichEmbed } = require('discord.js');
 
 var prefix = ("$")
-      
+
+
+var role = member.guild.roles.find(`name`, `corona`);
+
+
+
+
+    member.addRole(role)   
       
 bot.on('ready',function() {
       bot.user.setActivity(`vous contaminer`, { type: `PLAYING`})
