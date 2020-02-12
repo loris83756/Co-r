eaf235284af1eca9b65c485e57b29449d5f31aad
loris83756 <43618944+loris83756@users.noreply.github.com>
2020-02-12 11:30:59 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,12 +3,10 @@ const bot = new Discord.Client();
 const { Client, RichEmbed } = require('discord.js');
 var prefix = ("$")
 
-
 bot.on("guildMemberAdd" , member => {
     member.guild.channels.find("name","👋nouveaux-départs").send(`** Bienvenue ${member} tu est maintenant infecté.**`)
 })
     
-
 bot.on('ready',function() {
       bot.user.setActivity(`vous contaminer`, { type: `PLAYING`})
     console.log("Connecter");
@@ -24,8 +22,6 @@ bot.on(`guildMemberAdd`,member => {
         message.reply("gegfjegfjfejkfejkfjke");
         console.log(`Commade bonjour effectué par: ${message.author} sur ${message.guild.name} a ${message.createdAt}`)
     }
-
-
         
 }); 
 bot.login(process.env.TOKEN);
